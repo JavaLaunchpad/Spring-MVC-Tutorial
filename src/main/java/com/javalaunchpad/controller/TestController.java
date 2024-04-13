a2b4240205a8e9d64370eaceaba26b2df72f2652
hamza <hamza.nassour13@gmail.com>
2024-04-13 15:37:53 +0000

    keep all the article scenarios

--- src/main/java/com/javalaunchpad/controller/TestController.java
+++ src/main/java/com/javalaunchpad/controller/TestController.java
@@ -3,9 +3,12 @@ package com.javalaunchpad.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @Controller
 public class TestController {
+
+   // DispatcherServlet
 
     @RequestMapping("/test")
     @ResponseBody
